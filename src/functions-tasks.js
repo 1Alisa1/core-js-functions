6b8c047e45f8f2a77fdca6fdf1ac9f8cc5007dad
Alisa <Alisa.dragun@mail.ru>
2024-04-03 20:30:35 +0200

    complete the rest of tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -92,8 +92,18 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  return (x) => {
+    let result = null;
+    if (args[0] && args[1] && args[2]) {
+      result = args[0] * x ** 2 + args[1] * x + args[2];
+    } else if (args[0] && args[1]) {
+      result = args[0] * x + args[1];
+    } else if (args[0]) {
+      [result] = args;
+    }
+    return result;
+  };
 }
 
 /**
@@ -176,8 +186,20 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsStr = args.map((arg) => JSON.stringify(arg)).join(',');
+    const messageStart = `${func.name}(${argsStr}) starts`;
+    const messageEnd = `${func.name}(${argsStr}) ends`;
+
+    logFunc(messageStart);
+
+    const result = func.apply(this, args);
+
+    logFunc(messageEnd);
+
+    return result;
+  };
 }
 
 /**
@@ -193,8 +215,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args) => {
+    return fn(...args1, ...args);
+  };
 }
 
 /**
@@ -214,8 +238,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom;
+
+  return () => {
+    count += 1;
+    return count - 1;
+  };
 }
 
 module.exports = {
